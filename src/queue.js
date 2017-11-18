e242666733ccce901500f5f6357167926c7034f9
antarid <tarand007@gmail.com>
2017-11-18 10:47:10 +0300

    ???

--- src/queue.js
+++ src/queue.js
@@ -2,7 +2,8 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.maxSize = (maxSize instanceof undefined)? 30 : maxSize;
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
